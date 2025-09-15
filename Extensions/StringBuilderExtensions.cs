1ff7999aa4c5148e4e83999f5e6ab6e4ad59b8fb
AWildErin <sharpian126@gmail.com>
2025-09-15 06:31:23 +0100

    feat: Add string builder extensions

---
+++ Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace AWildErin.Utility.Extensions;
+
+public static class StringBuilderExtensions
+{
+	private static string GetIndentString( int IndentLevel )
+	{
+		return new string( '\t', IndentLevel );
+	}
+
+	public static StringBuilder AppendIndentedLine(this StringBuilder StringBuilder, int Indent, string Text)
+	{
+		return StringBuilder.AppendLine($"{GetIndentString(Indent)}{Text}");
+	}
+
+	public static StringBuilder AppendIndented( this StringBuilder StringBuilder, int Indent, string Text )
+	{
+		return StringBuilder.Append( $"{GetIndentString( Indent )}{Text}" );
+	}
+}
